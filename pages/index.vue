806c41c59b80d998510006db6a46e97c7cab3b5f
Charles DZADU <dkccharlie@gmail.com>
2023-07-22 12:15:31 +0000

    :heart: Home page non responsive done

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,33 @@
 <template>
-	<div>
-		<h1></h1>
+	<div class=" w-full h-full flex p-4">
+		<div class="w-[300px] ">
+
+		</div>
+		<div class="flex justify-center items-center gap-24" >
+			<div>
+				<div class="flex flex-col gap-3">
+					<h2>Hi all. I am</h2>
+					<h1 class="text-6xl text-white font-bold">Charles DZADU</h1>
+					<h3 class="text-purple text-2xl">> Fullstack Developper, UX/UI Designer</h3>
+				</div>
+				<div class="h-48">
+
+				</div>
+				<div>
+					<span>// Complete the game to continue</span>
+					<br>
+					<span>// You can also check my Github page </span>
+					<br>
+					<br>
+					<a href="https://github.com/charlesdzadu" class="text-white"> <span class="text-purple">const</span> <span class="text-green-light">githubLink</span> = <span class="text-orange">"https://github.com/charlesdzadu"</span> </a>
+				</div>
+			</div>
+			<div>
+				<div class="h-[500px] w-[500px] border">
+
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
