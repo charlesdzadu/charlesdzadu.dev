5515cb78be840cd3f15e17ebf33a736da1fafde0
Charles DZADU <dkccharlie@gmail.com>
2023-10-22 23:07:06 +0000

    fix: Remove under construction bar

--- components/Header.vue
+++ components/Header.vue
@@ -5,10 +5,10 @@
 				<NuxtLink to="/" class=" hover:text-white transition-colors  w-[300px]">charles-dzadu</NuxtLink>
 				<HeaderMenu></HeaderMenu>
 			</div>
-			<span class="mt-3 text-orange font-bold text-xl">Please note that this site is still under construction</span>
 			<div class="flex items-center">
 				<LineV></LineV>
-				<a href="https://drive.google.com/file/d/14wsGOjBPNMI6GHB7PXNwruZz31lDlWN1/view?usp=sharing" target="_blank" class="flex p-3 gap-2 items-center">
+				<a href="https://drive.google.com/file/d/14wsGOjBPNMI6GHB7PXNwruZz31lDlWN1/view?usp=sharing" target="_blank"
+					class="flex p-3 gap-2 items-center">
 					<i class="ri-download-cloud-fill ri-lg"></i>
 					<span>download-cv</span>
 				</a>
